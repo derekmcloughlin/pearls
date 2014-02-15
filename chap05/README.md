c229b4313525e1f37f16daf281f4c12c007831ca
Derek McLoughlin <derek.mcloughlin@gmail.com>
2014-02-15 11:47:46 +0000

    First step in chapt 5

--- chap05/README.md
+++ chap05/README.md
@@ -7,7 +7,7 @@ Lambert's Algorithm
 For the purposes of this chapter we need a monotonic function ⊕ and it's 
 negation ⊖ . Let's use ordinary addition and subtraction for this.
 
-The properties are satisfied:
+The following properties are satisfied:
 
     x <= x', y <= y' => x + y <= x' + y'
     x + y = x - negate y
@@ -27,9 +27,42 @@ ghci> negate (-1)
 For testing:
 
 ```haskell
-let xs = [1, 3, 5, 7, 9, 11]
-let ys = [20, 30, 40, 50, 60, 70]
+let xs = [7, 3, 1, 9, 5]
+let ys = [40, 20, 30, 60, 50]
 ```
 
+To test if a list is sorted we could hand-roll our own function:
+
+```haskell
+isSorted xs = (sort xs) == xs
+```
+
+Alternatively we can use the `isSorted` function from Data.List.Ordered.
+
+First Attempt
+-------------
+
+```haskell
+sortsums :: Ord a => Num a => [a] -> [a] -> [a]
+sortsums xs ys = map fst (sortsubs xs (map negate ys))
+
+sortsubs :: Ord a => Num a => [a] -> [a] -> [(a, (Integer, Integer))]
+sortsubs xs ys = sort (subs xs ys)
+
+subs :: Num a => [a] -> [a] -> [(a, (Integer, Integer))]
+subs xs ys = [ (x - y, (i, j)) | (x, i) <- zip xs [1..], (y, j) <- zip ys [1..]]
+```
+
+Code is in chap05a.hs.
+
+```haskell
+ghci> :l chap05a.hs
+ghci> let a = sortsums xs ys
+ghci> c
+[21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69]
+ghci> isSorted c
+True
+ghci>
+```
 
 
--- chap05/chap05a.hs
+++ chap05/chap05a.hs
@@ -1,11 +1,9 @@
 import Data.List
+import Data.List.Ordered
 import Data.Array
 
-xs = [1, 3, 5, 7, 9]
-ys = [20, 30, 40, 50, 60]
-
-subs :: Num a => [a] -> [a] -> [(a, (Integer, Integer))]
-subs xs ys = [ (x - y, (i, j)) | (x, i) <- zip xs [1..], (y, j) <- zip ys [1..]]
+xs = [7, 3, 1, 9, 5]
+ys = [40, 20, 30, 60, 50]
 
 sortsums :: Ord a => Num a => [a] -> [a] -> [a]
 sortsums xs ys = map fst (sortsubs xs (map negate ys))
@@ -13,25 +11,5 @@ sortsums xs ys = map fst (sortsubs xs (map negate ys))
 sortsubs :: Ord a => Num a => [a] -> [a] -> [(a, (Integer, Integer))]
 sortsubs xs ys = sort (subs xs ys)
 
-table :: Ord a => Num a => [a] -> [a] -> [(Integer, Integer, Integer)]
-table xs ys = map snd (map (tag 1) xxs `merge` map (tag 2) yys) 
-              where xxs = sortsubs xs xs
-                    yys = sortsubs ys ys 
-
-tag :: Integer -> (a, (Integer, Integer)) -> (a, (Integer, Integer, Integer))
-tag i (x,(j,k)) = (x,(i,j,k))
-
--- Merge two sorted lists to produce a sorted list
-merge :: Ord a => [a] -> [a] -> [a]
-merge xs [] = xs
-merge [] xs = xs
-merge a@(h:first) b@(c:second)
-        | h <= c = h:merge first b
-        | h > c = c:merge a second
-
--- mkArray :: [a] -> [a] -> Array [a]
-mkArray xs ys = array b (zip (table xs ys) [1..])
-                where b = ((1, 1, 1), (2, p, p))
-                      p = toInteger (max (length xs) (length ys)) 
-
-
+subs :: Num a => [a] -> [a] -> [(a, (Integer, Integer))]
+subs xs ys = [ (x - y, (i, j)) | (x, i) <- zip xs [1..], (y, j) <- zip ys [1..]]
